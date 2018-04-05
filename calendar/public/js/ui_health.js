d661ed47c3d7a75627ee74b4a7ceb6d797997a63
Sankalp Pandey <sankalp.pandey@hpe.com>
2018-04-05 14:34:03 +0530

    added static banner and api interface

--- calendar/public/js/ui_health.js
+++ calendar/public/js/ui_health.js
@@ -1,8 +1,26 @@
-app.controller('UIHealthCtrl', function ($scope, $location) {
+app.controller("UIHealthCtrl", function(
+	$scope,
+	$location,
+	$rootScope,
+	$http,
+	socket
+) {
 
-    $scope.tabs = [
-        { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
-        { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
-    ];
-    
+	$scope.api = {"url": "", "username": "", "password": ""};
+
+	var getBannerInfo = function() {
+		$http.get("/coc/cdps/" + $rootScope.selectedCDPID).then(function(res) {
+			var cdp_data = res.data[0];
+			$scope.cdp_banner_info = cdp_data.banner_info;
+		});
+	};
+
+	$scope.submit_api_form = function(data){
+
+	}
+	getBannerInfo();
+
+	socket.on("add", function(data) {
+		$scope.cdp_banner_info = data.banner_info;
+	});
 });
