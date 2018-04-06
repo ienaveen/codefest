2c4cabc474b49e61c6ae29f074e1919707616600
Naveen Kondiparti <naveen.kondiparti@hpe.com>
2018-04-06 18:56:57 +0530

    added coc dashboard style changes

--- calendar/public/js/directives/card.directive.js
+++ calendar/public/js/directives/card.directive.js
@@ -8,12 +8,26 @@ app.directive('card', function () {
 				<button type="button" class="close" aria-label="Close" ng-click="clickclose()">
 					<span aria-hidden="true">&times;</span>
 				</button>
-                <div class="card-body">
-                    <h4 class="card-title">{{cardobj.cdp_details.title}}</h4>
-                    <p class="card-text">{{cardobj.cdp_details.desc}}</p>
-                    <p class="card-text">{{cardobj.cdp_details.ip}}</p>
-                    <p class="card-text">{{cardobj.cdp_details.version}}</p>
-                    <p class="card-text">{{cardobj.cdp_details.status}}</p>
+                <div class="card-body">                
+                    <center><h4 class="card-title">{{cardobj.cdp_details.title}}</h4></center>
+                    <center><p class="card-text">{{cardobj.cdp_details.desc}}</p></center>
+                    <ul>
+                       <li>
+                       		<span class="card-label">Host:</span>		
+                    		<span class="card-text">{{cardobj.cdp_details.ip}}</span>
+                      <li>
+                      <li>
+                      		<span class="card-label">Version:</span>		
+                    		<span class="card-text">{{cardobj.cdp_details.version}}</span>
+                    </li>
+                    <li>
+                    	<span class="card-label">Status:</span>
+                    	<span class="card-text" ng-class="{'color-red':cardobj.cdp_details.status == 'Unstable','color-green':cardobj.cdp_details.status == 'Stable','color-blue':cardobj.cdp_details.status == 'Upgrading','color-orange':cardobj.cdp_details.status == 'Partial-Stable'}">{{cardobj.cdp_details.status}}</span>
+                    </li>
+                    <li>
+                    	<span class="card-label">Channel:</span>
+                    	<span class="card-text">{{cardobj.cdp_details.channel_name}}</span>
+                    </li>
                 </div>
             </div>
         `,
